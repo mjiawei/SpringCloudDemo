da8c7e740bd9d8e712e5b50f2c1102c05eac5034
jiawei.song <jiawei.song@bkjk.com>
2018-04-02 14:40:52 +0800

    add zipkin sluth

--- Service-Hi/src/main/java/com/jiawei/servicehi/ServiceHiApplication.java
+++ Service-Hi/src/main/java/com/jiawei/servicehi/ServiceHiApplication.java
@@ -1,27 +1,90 @@
 package com.jiawei.servicehi;
 
+import com.google.common.util.concurrent.*;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.*;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 @RestController
 public class ServiceHiApplication {
 
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceHiApplication.class, args);
+		//testGuava();
 	}
 
 	@Value("${server.port}")
 	String port;
+
 	@RequestMapping("/hi")
 	public String home(@RequestParam String name) {
 		return "hi "+name+",i am from port:" +port;
 	}
+
+
+
+
+	public static void testGuava(){
+
+		ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(0, Integer.MAX_VALUE,
+				60L, TimeUnit.SECONDS,
+				new SynchronousQueue<Runnable>());
+		ListeningExecutorService executorService = MoreExecutors.listeningDecorator(threadPoolExecutor);
+		final ListenableFuture<Integer> listenableFuture = executorService.submit(new Callable<Integer>() {
+			@Override
+			public Integer call() throws Exception {
+				System.out.println("call execute..");
+				TimeUnit.SECONDS.sleep(1);
+				return 7;
+			}
+		});
+	}
+
+
+	private static final Logger LOG = Logger.getLogger(ServiceHiApplication.class.getName());
+
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Bean
+	public RestTemplate getRestTemplate(){
+		return new RestTemplate();
+	}
+
+	@RequestMapping("/hello")
+	public String callHome(){
+		LOG.log(Level.INFO, "calling trace service-hi  ");
+		return restTemplate.getForObject("http://localhost:8989/miya", String.class);
+	}
+	@RequestMapping("/info")
+	public String info(){
+		LOG.log(Level.INFO, "calling trace service-hi ");
+		return "i'm service-hi";
+
+	}
+
+	@Bean
+	public AlwaysSampler defaultSampler(){
+		return new AlwaysSampler();
+	}
+
 }
